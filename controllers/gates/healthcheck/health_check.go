a2955cb07ed7c82e68f18578ead86d016f14b0fd
Kevin McDermott <bigkevmcd@gmail.com>
2023-05-15 08:20:17 +0100

    Start of health checks.

--- controllers/gates/healthcheck/health_check.go
+++ controllers/gates/healthcheck/health_check.go
@@ -18,17 +18,38 @@ package healthcheck
 
 import (
 	"context"
+	"net/http"
 
 	deployerv1 "github.com/gitops-tools/kustomization-auto-deployer/api/v1alpha1"
+	"github.com/gitops-tools/kustomization-auto-deployer/controllers/gates"
+	"github.com/go-logr/logr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
+
+// GateFactory is a function for creating per-reconciliation gates for
+// the HealthCheckGate.
+func GateFactory(l logr.Logger, _ client.Client) gates.Gate {
+	return NewGate(l)
+}
+
+// NewGate creates and returns a new HealthCheck gate.
+func NewGate(l logr.Logger, httpClient *http.Client) *HealthCheckGate {
+	return &HealthCheckGateGate{
+		Logger:     l,
+		HTTPClient: httpClient,
+	}
+}
 
 // HealthCheckGate checks an HTTP endpoint and is open if the gate returns a 200
 // response.
 //
 // Any other response, is closed.
 type HealthCheckGate struct {
+	Logger     logr.Logger
+	HTTPClient *http.Client
 }
 
+// Check returns true if the URL returns a 200 response.
 func (g HealthCheckGate) Check(context.Context, deployerv1.KustomizationGate, deployerv1.GatedKustomizationDeployer) (bool, error) {
 	return false, nil
 }
